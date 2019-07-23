55682adebc05919b3d92ffd9e965cba747b0b2b0
Henrique Pereira <hlcpereira@outlook.com.br>
2019-07-23 09:27:30 -0300

    sdm710-common: Initial common-tree files
    * CPU Arch flags taken from Bonito/Sargo
    
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

---
+++ README.md
@@ -0,0 +1,5 @@
+Copyright (C) 2019 - The PixelExperience Project
+
+Common device tree for Xiaomi SDM710 based devices
+==============
+
---
+++ sdm710.mk
@@ -0,0 +1,66 @@
+#
+# Copyright 2019 The PixelExperience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/xiaomi/sdm710-common/sdm710-common-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    tinymix
+
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan \
+    vendor.display.config@1.0
+
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
